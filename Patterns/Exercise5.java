9b384b63e2be25b2a33b8219fe9280b2fcaaaf0b
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-06-26 17:01:06 +0530

    code for printing half pyramid(180 degree rotated)

--- Patterns/Exercise5.java
+++ Patterns/Exercise5.java
@@ -1,13 +1,13 @@
 
 //code for printing half pyramid(180 degree rotated)
 
-class Exercise5  {
+class Exercise5 {
     public static void main(String[] args) {
-        for(int i=1; i<=5; i++){
-            for(int j=1; j<=5-i; j++){
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
                 System.out.print(" ");
             }
-            for(int j=1; j<=i; j++){
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
