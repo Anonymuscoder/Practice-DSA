24e0c3f89b5a6b21d9329f3f72668077a75097e4
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-06-26 23:50:51 +0530

    updated code Exercise14.java

--- Patterns/Exercise14.java
+++ Patterns/Exercise14.java
@@ -3,20 +3,20 @@
 
 public class Exercise14 {
     public static void main(String[] args) {
-        for(int i=1; i<=4; i++){
-            for(int j=1; j<=4-i; j++){
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4 - i; j++) {
                 System.out.print(" ");
             }
-            for(int j=1; j<=2*i-1; j++){
+            for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        for(int i=4; i>=1; i--){
-            for(int j=1; j<=4-i; j++){
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 1; j <= 4 - i; j++) {
                 System.out.print(" ");
             }
-            for(int j=1; j<=2*i-1; j++){
+            for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
