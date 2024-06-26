23b26b8cd7f563fa0ab672615d8829fc2842be48
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-06-26 16:07:24 +0530

    code for printing a solid rectangle

---
+++ Patterns/Exercise.java
@@ -0,0 +1,13 @@
+
+//code for printing a solid rectangle
+
+class Exercise {
+    public static void main(String[] args) {
+        for(int i=1; i<=4; i++){
+            for(int j=0;j<=10;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }    
+}
