72d5e581367519a97cbded9b9e068d71df50b169
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-06-26 22:57:37 +0530

    code for printing solid rhombus

---
+++ Patterns/Exercise11.java
@@ -0,0 +1,21 @@
+
+//code for printing solid rhombus
+
+
+class Exercise11 {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+
+            for(int j=1; j<=5-i;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1; j<=5; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+    
+}
