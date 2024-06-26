3400b4f8e99bbf6483af90230455ff59e85734ec
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-06-26 23:50:09 +0530

    updated code Exercise14.java

--- Patterns/Exercise14.java
+++ Patterns/Exercise14.java
@@ -1,33 +1,25 @@
 
 //code for printing Diamond Pattern
 
-class Exercise14 {
+public class Exercise14 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 1; j <= 4 - i; j++) {
+        for(int i=1; i<=4; i++){
+            for(int j=1; j<=4-i; j++){
                 System.out.print(" ");
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            for (int j = 2; j <= i; j++) {
+            for(int j=1; j<=2*i-1; j++){
                 System.out.print("*");
             }
             System.out.println();
         }
-        for (int i = 4; i >= 1; i--) {
-            for (int j = 1; j <= 4 - i; j++) {
+        for(int i=4; i>=1; i--){
+            for(int j=1; j<=4-i; j++){
                 System.out.print(" ");
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            for (int j = 2; j <= i; j++) {
+            for(int j=1; j<=2*i-1; j++){
                 System.out.print("*");
             }
             System.out.println();
         }
-
     }
-
 }
