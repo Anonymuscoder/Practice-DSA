81e2e6cdcc3873f6959125aec8ad1adfcce4aad0
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-06-26 17:32:58 +0530

    code for printing 0-1 Triangle

---
+++ Patterns/Exercise9.java
@@ -0,0 +1,17 @@
+
+//code for printing 0-1 Triangle
+
+class Exercise9 {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++){
+                int sum= i+j;
+                if(sum%2==0)
+                    System.out.print(1);
+                else
+                    System.out.print(0);
+            }
+            System.out.println();
+        }
+    }
+}
