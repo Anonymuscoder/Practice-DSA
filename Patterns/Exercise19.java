b6c6130a8f3e68366743594d7ab60923ef718240
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-06-28 17:51:34 +0530

    code for printing half pyramid using numbers

---
+++ Patterns/Exercise19.java
@@ -0,0 +1,13 @@
+//code for printing half pyramid using numbers
+
+class Exercise19 {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1; i<=n; i++){
+            for(int j=i; j<=4; j++){  
+                System.out.print(i);   
+            }
+            System.out.println();
+        }
+    }
+}
