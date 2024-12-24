d6d13c9fb07ed0a01da32393df1c250f445239f9
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-12-24 14:14:24 +0530

    Write a recursive function to print all numbers from 1 to n.

---
+++ Recursion/practice12.java
@@ -0,0 +1,17 @@
+////Write a recursive function to print all numbers from 1 to n.
+
+class practice12 {
+    
+    public static void inc(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        inc(n-1);
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        inc(5);
+    }
+}
